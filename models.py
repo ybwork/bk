817f987a9b2194056ca3b62df1e437a9f7f32e79
yetbetter <costilek@gmail.com>
2019-01-22 06:56:27 +0300

    Добавил таблицы для банка и клиента

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Table
 
 db = SQLAlchemy()
 
@@ -7,3 +8,16 @@ class App(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     key = db.Column(db.String(16), unique=True, nullable=False)
+
+
+class Bank(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    balance = db.Column(db.DECIMAL(10, 2), nullable=False)
+
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+
