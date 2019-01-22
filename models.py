44cc3c2de952f80ebb1fb4d15487d9f1f201e93d
Ilya Kaduk <kaduk2593@gmail.com>
2019-01-22 10:37:54 +0300

    Исправил ошибку с добавлением .pyc в .gitignore

--- models.py
+++ models.py
@@ -21,3 +21,53 @@ class Client(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(255), nullable=False)
 
+
+bank_client = db.Table(
+    'bank_client',
+    db.Column(
+        'id',
+        db.Integer,
+        primary_key=True
+    ),
+    db.Column(
+        'bank_id',
+        db.Integer,
+        db.ForeignKey('bank.id'),
+        primary_key=True
+    ),
+    db.Column(
+        'client_id',
+        db.Integer,
+        db.ForeignKey('client.id'),
+        primary_key=True
+    ),
+    db.Column(
+        'number_invoice',
+        db.String(5),
+        nullable=False
+    ),
+    db.Column(
+        'balance',
+        db.DECIMAL(10, 2),
+        nullable=False
+    )
+)
+
+
+class StatusOperation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+
+class Operation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(5), nullable=False)
+    amount_money = db.Column(db.DECIMAL(10, 2), nullable=False)
+    number_invoice_provider = db.Column(db.String(5), nullable=False)
+    number_invoice_reciever = db.Column(db.String(5), nullable=False)
+    status_id = db.Column(
+        db.Integer,
+        db.ForeignKey('status_operation.id'),
+        nullable=False
+    )
+
--- views.py
+++ views.py
@@ -1,13 +1,17 @@
+import os
+
 from flask import Blueprint
+
+from utils import send_json_response
 
 views = Blueprint('views', __name__)
 
-from middleware import *
+# from middleware import *
 
 
-@views.route('/', methods=['POST'])
+@views.route('/', methods=['GET', 'POST'])
 def home():
     return send_json_response(
-        message={'message': 'home'},
+        message={'message': os.urandom(16).hex()},
         status_code=200
     )
